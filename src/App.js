d37fe8f7b8fb59e862a38b73a0ab3a9ad339bd8b
Virochaan <39714123+VirochaanRG@users.noreply.github.com>
2022-05-24 11:36:25 -0400

    Adding Navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import './App.css';
+import ResponsiveAppBar from './components/Navbar';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ResponsiveAppBar/>
+      <div><h1>HELLO</h1></div>
+      <div><h1>HELLO</h1></div><div><h1>HELLO</h1></div>
+      <div><h1>HELLO</h1></div>
+      <div><h1>HELLO</h1></div><div><h1>HELLO</h1></div>
+      <div><h1>HELLO</h1></div>
+      <div><h1>HELLO</h1></div>
+      <div><h1>HELLO</h1></div><div><h1>HELLO</h1></div>
+      <div><h1>HELLO</h1></div>
+      <div><h1>HELLO</h1></div>
+      <div><h1>HELLO</h1></div><div><h1>HELLO</h1></div>
+      <div><h1>HELLO</h1></div>
+      <div><h1>HELLO</h1></div>
     </div>
   );
 }
